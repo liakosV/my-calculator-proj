ef9b1a2fca218eefcabc606a1739866a4abe5596
LiakosV <billiak@hotmail.com>
2024-12-16 19:42:48 +0200

    project created

---
+++ src/gr/aueb/cf/ch2/subCalc.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch2;
+
+public class subCalc {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
+
+        result = sub(num1, num2);
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
